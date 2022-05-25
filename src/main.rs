3d3f95322809fc4a64598f3f57fef1cd0b26a120
Max Dayton <daytonmaximus@gmail.com>
2022-05-24 19:27:34 -0600

    index route and mount

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+        POST /
+
+            accepts raw data in the body of the request and responds with a URL of a page containing the body's content.
+
+        GET /<id>
+
+            responds with the content of the page with the given ID.
+    "
+}
+

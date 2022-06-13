48b5c5317b3277d81244e7e890f51e77589a352b
Max Dayton <daytonmaximus@gmail.com>
2022-06-13 17:42:58 -0600

    using PasteId type and impl of FromParam

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[macro_use] extern crate rocket;
 
 mod paste_id;
+
 use paste_id::PasteId;
+use rocket::tokio::fs::File;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, retrieve])
 }
 
 #[get("/")]
@@ -22,3 +24,7 @@ fn index() -> &'static str {
     "
 }
 
+#[get("/<id>")]
+async fn retrieve(id: PasteId<'_>) -> Option<File> {
+    File::open(id.file_path()).await.ok()
+}
--- src/paste_id.rs
+++ src/paste_id.rs
@@ -1,9 +1,23 @@
 use std::borrow::Cow;
 use std::path::{Path, PathBuf};
+use rocket::request::FromParam;
 
 use rand::{self, Rng};
 
 pub struct PasteId<'a>(Cow<'a, str>);
+
+
+/// Returns an instance of `PasteId` if the path segment is a valid ID.
+/// Otherwise returns the invalid ID as the `Err` value.
+impl<'a> FromParam<'a> for PasteId<'a> {
+    type Error = &'a str;
+
+    fn from_param(param: &'a str) -> Result<Self, Self::Error> {
+        param.chars().all(|c| c.is_ascii_alphanumeric() || c == '-')
+            .then(|| PasteId(param.into()))
+            .ok_or(param)
+    }
+}
 
 impl PasteId<'_> {
     pub fn new(size: usize) -> PasteId<'static> {
